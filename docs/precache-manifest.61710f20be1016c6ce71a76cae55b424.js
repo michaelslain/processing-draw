9cd835b5842fe54acc08d75b4e9c3c13243f1c96
michaelslain <misha.slain@icloud.com>
2020-08-29 15:55:05 -0700

    Fixed github pages errors

---
+++ docs/precache-manifest.61710f20be1016c6ce71a76cae55b424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dce124a4752213805ac589408903eee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8f2079abf312bed87682",
+    "url": "./static/css/main.456c3ce5.chunk.css"
+  },
+  {
+    "revision": "39f93d568339bc5df185",
+    "url": "./static/js/2.74084fad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.74084fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f2079abf312bed87682",
+    "url": "./static/js/main.c57b2957.chunk.js"
+  },
+  {
+    "revision": "d17524cb3338c5e95515",
+    "url": "./static/js/runtime-main.4beda586.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc197e96d7ddb1c892b71ba70c6677b7.js"
+  "./precache-manifest.61710f20be1016c6ce71a76cae55b424.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
